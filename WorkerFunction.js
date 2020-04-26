5bf0da24ad92b0a9f4da8ea1c49ac650b6b3cfe2
Javier Marquez <javi@arqex.com>
2020-04-26 19:40:58 +0200

    Defines worker function differently depending on support. Added closure to not polute globalThis

--- WorkerFunction.js
+++ WorkerFunction.js
@@ -1,63 +1,88 @@
-var WORKER_ENABLED = !!(typeof window !== 'undefined' && window.URL && window.Blob && window.Worker);
+(function( glob ) {
+  var WORKER_ENABLED = areWorkersAvailable();
 
-var wrapper = function(){
-  // user function already defined as uf
-
-  var done = function( result ){
-    self.postMessage( result );
-    self.close();
+  var wrapper = function(){
+    // user function already defined as uf
+    var done = function( result ){
+      self.postMessage( result );
+      self.close();
+    }
+  
+    self.onmessage = function( msg ){
+      var args = msg.data.slice();
+      args.push( done );
+      uf.apply(self, args);
+    };
   }
-
-  self.onmessage = function( msg ){
-    var args = msg.data.slice();
-    args.push( done );
-    uf.apply(self, args);
-  };
-}
-
-function WorkerFunction( fn ){
-  if (!WORKER_ENABLED) {
-    // Older browsers will run the function in the main thread.
-    return function () {
-      var args = Array.prototype.slice.call(arguments, 0);
-
-      return new Promise(function (resolve, reject) {
-        try {
-          args.push(resolve);
-          fn.apply(self, args);
-        }
-        catch (e) {
-          reject(re);
-        }
-      });
+  
+  var WorkerFunction;
+  if( WORKER_ENABLED ){
+    WorkerFunction = function(fn) {
+      return function WorkedFunction(){
+        var args = Array.prototype.slice.call( arguments, 0 );
+    
+        return new Promise( function( resolve, reject ){
+          var fnSrc = 'var uf = ' + fn.toString() + ';\n';
+          var wSrc = 'var w = ' + wrapper.toString() + ';\n' + 'w();';
+          var worker = new SrcWorker( fnSrc + wSrc );
+    
+          worker.onmessage = function( result ){
+            resolve( result.data );
+          };
+          worker.onerror = function( err ){
+            reject( err );
+          }
+    
+          worker.postMessage( args );
+        });
+      }
     }
   }
-
-  return function(){
-    var args = Array.prototype.slice.call( arguments, 0 );
-
-    return new Promise( function( resolve, reject ){
-      var fnSrc = 'var uf = ' + fn.toString() + ';\n';
-      var wSrc = 'var w = ' + wrapper.toString() + ';\n' + 'w();';
-      var worker = new SrcWorker( fnSrc + wSrc );
-
-      worker.onmessage = function( result ){
-        resolve( result.data );
-      };
-      worker.onerror = function( err ){
-        reject( err );
+  else {
+    console.warn('worker-function: BROWSER NOT SUPPORTED - falling back to non-worker methods')
+    WorkerFunction = function(fn) {
+      return function NonWorkedFunction() {
+        var args = Array.prototype.slice.call(arguments, 0);
+  
+        return new Promise(function (resolve, reject) {
+          try {
+            args.push(resolve);
+            fn.apply(self, args);
+          }
+          catch (e) {
+            reject(re);
+          }
+        });
       }
-
-      worker.postMessage( args );
-    });
+    }
   }
-}
+  
+  function areWorkersAvailable(){
+    var available = !!(typeof window !== 'undefined' && window.URL && window.Blob && window.Worker);
+    if( available ){
+      // One more test
+      try {
+        var worker = new SrcWorker('');
+        worker.terminate();
+      }
+      catch( err ){
+        return false;
+      }
+    }
+    return available;
+  }
+  
+  function SrcWorker( src ){
+    return new window.Worker( window.URL.createObjectURL(
+      new window.Blob([ src ], { type: "text/javascript" })
+    ));
+  }
+  
+  if( typeof module !== 'undefined' ){
+    module.exports = WorkerFunction;
+  }
+  else if (glob ){
+    glob.WorkerFunction = WorkerFunction;
+  }
+})(this);
 
-function SrcWorker( src ){
-  return new window.Worker(window.URL.createObjectURL(
-    new window.Blob([ src ], { type: "text/javascript" })
-  ));
-}
-
-if(typeof module !== 'undefined')
-  module.exports = WorkerFunction;
